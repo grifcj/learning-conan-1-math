930a3a43c05aa5e46167bbda423d05189f978b29
Maria Ninos <marianinos@Marias-MBP.attlocal.net>
2019-10-13 13:15:15 -0700

    Use impl in base conan file

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, python_requires
 
 base = python_requires("conanbase/1.0.0-nightly@grifcj/dev")
 
-class MathConan(ConanFile):
+class MathConan(base.get_conanfile()):
     name = "math"
     version = "1.0.0-nightly"
     scm = {
@@ -15,25 +15,6 @@ class MathConan(ConanFile):
     build_requires = (
             "cmake_extensions/1.0.0-nightly@grifcj/dev",
             "gtest/1.8.1@bincrafters/stable")
-    generators = "cmake_paths"
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        conan_paths = os.path.join(self.build_folder, "conan_paths.cmake")
-        cmake.definitions["CONAN_PACKAGE_VERSION"] = self.version.split('-')[0]
-        cmake.definitions["CMAKE_FIND_PACKAGE_PREFER_CONFIG"] = "TRUE"
-        cmake.definitions["CMAKE_PROJECT_INCLUDE"] = conan_paths
-        cmake.generator = "Ninja"
-        cmake.configure()
-        return cmake
-
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
-        cmake.test()
-
-    def package(self):
-        cmake = self._configure_cmake()
-        cmake.install()
 
 
