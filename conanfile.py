82f975b3fa5877e4a5300290950814adb2f9b5ec
Maria Ninos <marianinos@Marias-MBP.attlocal.net>
2019-10-27 23:03:17 -0700

    Try new package format that relies on conan generated targets

--- conanfile.py
+++ conanfile.py
@@ -6,6 +6,9 @@ base = python_requires("conanbase/1.0.0-nightly@grifcj/dev")
 class MathConan(base.get_conanfile()):
     name = "math"
     version = "1.0.0-nightly"
+    url = "https://github.com/grifcj/cmake-math"
+    license = "MIT"
+    description = "A learning math library"
     scm = {
         "type": "git",
         "url": "https://github.com/grifcj/cmake-math",
@@ -16,9 +19,6 @@ class MathConan(base.get_conanfile()):
             "cmake_extensions/1.0.0-nightly@grifcj/dev",
             "gtest/1.8.1@bincrafters/stable")
 
-    def _make_cmake(self):
-        cmake = super()._make_cmake()
-        cmake.definitions["logger_DIR"] = ""
-        return cmake
-
+    def package_info(self):
+        self.cpp_info.libs = ["math"]
 
